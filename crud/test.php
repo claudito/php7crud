378ca0e93d11864b86543d6f5e0df0c86b13787a
claudito <1107000044@upci.edu.pe>
2019-01-03 19:31:26 -0500

    página test

--- crud/test.php
+++ crud/test.php
@@ -6,11 +6,48 @@
 
 <!-- Sweet Alert -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
 
 </head>
 <body>
+
+<script>
+
+//Alerta Mensaje Cargando
+/*
+swal({
+ 
+  title:"Cargando",
+  text: "Espere un momento no cierre la ventana",
+  imageUrl: 'img/loader2.gif',
+  showConfirmButton:false
+
+});*/
+
+/*
+tipos:
+success
+error
+info
+warning
+
+*/
+
+//Mensaje de Confirmación
+swal({
+ 
+  title:"Buen Trabajo",
+  text: "Registro Agregado",
+  type: 'success',
+  timer: 3000,
+  showConfirmButton:false
+
+});
+
+
+</script>
+
 	
 </body>
 </html>
