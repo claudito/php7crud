faef4bbd3fc9094a29148a94bd60038a373682a1
claudito <1107000044@upci.edu.pe>
2019-01-03 19:18:33 -0500

    sweet alert

---
+++ crud/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Sweet Alert</title>
+
+<!-- Sweet Alert -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+
+
+</head>
+<body>
+	
+</body>
+</html>
